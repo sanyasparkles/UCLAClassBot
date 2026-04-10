a43c09440073283d8732fb710f703ccfc8412d16
sanyasparkles <sanya13@ucla.edu>
2026-04-10 16:12:17 -0700

    pte bot

--- Bot.py
+++ Bot.py
@@ -6,25 +6,19 @@ from selenium.webdriver.chrome.options import Options
 from LogIn import ucla_login
 from Email import send_alert
 from Tracker import check_for_open_spots
+from PTE import start_brute_force
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-# chrome_options = Options()
-# chrome_options.add_argument("--headless=new")  
-# chrome_options.add_argument("--disable-gpu")   
-# chrome_options.add_argument("--window-size=1920,1080")
-
-# driver = webdriver.Chrome(
-#     service=Service(ChromeDriverManager().install()),
-#     options=chrome_options
-# )
+#03461, 21007,  40496, 60610, 80551
 
 try:
     ucla_login(driver)
-    driver.minimize_window()
-    check_for_open_spots(driver, "COM SCI 143", "https://sa.ucla.edu/ro/ClassSearch/Results?SubjectAreaName=Computer+Science+(COM+SCI)&CrsCatlgName=143+-+Data+Management+Systems&t=26S&sBy=subject&subj=COM+SCI&catlg=0143&cls_no=%25&undefined=Go&btnIsInIndex=btn_inIndex")
+    #driver.minimize_window()
+    start_brute_force("07878", "COM SCI 118", "https://sa.ucla.edu/ro/ClassSearch/Results?SubjectAreaName=Computer+Science+(COM+SCI)&CrsCatlgName=118+-+Computer+Network+Fundamentals&t=26S&sBy=subject&subj=COM+SCI&catlg=0118&cls_no=%25&undefined=Go&btnIsInIndex=btn_inIndex", driver)
+    #check_for_open_spots(driver, "COM SCI 143", "https://sa.ucla.edu/ro/ClassSearch/Results?SubjectAreaName=Computer+Science+(COM+SCI)&CrsCatlgName=143+-+Data+Management+Systems&t=26S&sBy=subject&subj=COM+SCI&catlg=0143&cls_no=%25&undefined=Go&btnIsInIndex=btn_inIndex")
     
-
+  
 
 finally:
     driver.quit()
